646da3e11b2bf18706c212cf55ff5944fc731a04
Mohini <mohinisuneja24@gmail.com>
2020-09-23 20:03:39 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:buddies/screens/loginScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: loginScreen(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
---
+++ lib/screens/loginScreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class loginScreen extends StatefulWidget {
+  @override
+  _loginScreenState createState() => _loginScreenState();
+}
+
+class _loginScreenState extends State<loginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("App")),
+    );
+  }
+}
